805d700229f6ed54a667cb968c63bae66ba807ac
Zane Stock <zanestock3@gmail.com>
2022-04-11 06:56:18 -0500

    commit tree 389cf13105359a8b5da9be958c35f2b9638a9f26

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    cats;
+    }
+
+function destructivelyPrependCat() {
+        cats.unshift("Bob");
+        cats;
+    }
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    cats;
+}
+
+function appendCat(name) {
+    let appendCat = [...cats, "Broom"];
+    return appendCat;
+}
+
+function prependCat(name) {
+    let prependCat = ["Arnold", ...cats];
+    return prependCat;
+}
+
+function removeLastCat() {
+    let removeLastCat = cats.slice(0,2);
+    return removeLastCat;
+  }
+
+  function removeFirstCat () {
+    let removeFirstCat = cats.slice(1,3);
+    return removeFirstCat;
+  }
